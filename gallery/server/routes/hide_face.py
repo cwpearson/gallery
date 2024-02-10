9feba86c69969791e5f55827059e6023e37872a2
Carl Pearson <me@carlpearson.net>
2024-02-10 05:50:48 -0700

    add unhide-face endpoint

--- gallery/server/routes/hide_face.py
+++ gallery/server/routes/hide_face.py
@@ -11,10 +11,11 @@ from gallery import model
 from gallery.model import Face
 
 
-bp = Blueprint("hide-face")
+bp_hide = Blueprint("hide-face")
+bp_unhide = Blueprint("unhide-face")
 
 
-@bp.post("/api/v1/hide-face")
+@bp_hide.post("/api/v1/hide-face")
 def bp_image(request: Request):
     print(f"at /api/v1/hide-face")
 
@@ -23,6 +24,25 @@ def bp_image(request: Request):
     with Session(model.get_engine()) as session:
         face = session.scalars(select(Face).where(Face.id == face_id)).one()
         face.hidden = True
+        face.hidden_reason = model.HIDDEN_REASON_MANUAL
+        session.commit()
+
+    model.update_labels()
+
+    redirect_to = request.headers.get("referer")
+    print(f"redirecting to {redirect_to}")
+    return redirect(redirect_to)
+
+
+@bp_unhide.post("/api/v1/unhide-face")
+def bp_image(request: Request):
+    print(f"at /api/v1/unhide-face")
+
+    face_id = int(request.form.get("face_id"))
+
+    with Session(model.get_engine()) as session:
+        face = session.scalars(select(Face).where(Face.id == face_id)).one()
+        face.hidden = False
         face.hidden_reason = model.HIDDEN_REASON_MANUAL
         session.commit()
 
